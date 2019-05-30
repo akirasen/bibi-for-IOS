adcc3eeb79e38838cd36f64b772d26d8238053d9
武建斌 <602168099@qq.com>
2019-05-30 15:49:55 +0800

    开发者

---
+++ ThridTools/BitsharesWallet/Operations/BaseOperation.h
@@ -0,0 +1,24 @@
+//
+//  BaseOperation.h
+//  BitsharesWallet
+//
+//  Created by flh on 2018/8/15.
+//  Copyright © 2018年 flh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ObjectToDataProtocol.h"
+@class PublicKey;
+@class AssetAmountObject;
+@class AssetObject;
+@interface BaseOperation : NSObject<ObjectToDataProtocol>
+
+/**
+ 操作所需的签名公钥
+ */
+@property (nonatomic, strong) NSArray <PublicKey *> *requiredAuthority;
+
+- (AssetAmountObject *)caculateFeeWithFeeDic:(NSDictionary *)feeDictionary payFeeAsset:(AssetObject *)asset;
+
+- (id)caculateFeeWithFeeDic1:(NSDictionary *)feeDictionary payFeeAsset:(id)asset;
+@end
